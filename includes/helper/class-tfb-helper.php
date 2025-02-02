ade5893aa7367d073072343f099844ae5cee56d6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-02 15:51:20 +0800

    Update class-tfb-helper.php

--- includes/helper/class-tfb-helper.php
+++ includes/helper/class-tfb-helper.php
@@ -13,6 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class TFBDashboard_Helper {
     public function __construct() {
+        add_filter( 'woocommerce_guest_order_payment_notice', '__return_false' );
         add_action('woocommerce_admin_order_data_after_order_details', array($this, 'show_all_custom_order_meta_in_custom_fields'), 10, 2);
     }
     /**
