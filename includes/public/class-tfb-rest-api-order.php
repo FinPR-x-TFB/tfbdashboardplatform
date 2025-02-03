2f9391cad06380469be133a631059973392a5bd0
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-03 15:43:07 +0800

    Update class-tfb-rest-api-order.php

--- includes/public/class-tfb-rest-api-order.php
+++ includes/public/class-tfb-rest-api-order.php
@@ -67,12 +67,15 @@ class TFBDashboard_Rest_API_Order {
     public function tfbdashboard_validate_custom_order_fields( $prepared_post, $request ) {
         $required_fields = array( 'challengePricingId', 'stageId', 'userEmail', 'brandId' );
         foreach ( $required_fields as $field ) {
-            if ( empty( $request[ $field ] ) ) {
+            // Use get_param() to retrieve the value.
+            $value = $request->get_param( $field );
+            if ( empty( $value ) ) {
                 return new WP_Error( 'rest_order_missing_field', sprintf( __( '%s is required.', 'tfbdashboard' ), $field ), array( 'status' => 400 ) );
             }
         }
         return $prepared_post;
     }
+
 
     public function tfbdashboard_save_custom_order_fields( $order, $request ) {
         $custom_fields = array( 'challengePricingId', 'stageId', 'userEmail', 'brandId' );
